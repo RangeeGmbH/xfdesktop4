319a40934c456942ca431594d5ede79b5ddac93c
René Vögeli <rvoegeli@rangee.com>
2024-10-18 11:46:18 +0200

    Disable all hotkeys

--- src/xfdesktop-file-icon-manager.c
+++ src/xfdesktop-file-icon-manager.c
@@ -2581,6 +2581,8 @@ xfdesktop_file_icon_manager_key_press(GtkWidget *widget,
                                       GdkEventKey *evt,
                                       gpointer user_data)
 {
+    return FALSE;
+
     XfdesktopFileIconManager *fmanager = XFDESKTOP_FILE_ICON_MANAGER(user_data);
     GList *selected;
     gboolean force_delete = FALSE;
